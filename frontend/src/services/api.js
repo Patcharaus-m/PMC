9085c8b8040bee84312a3d9ccb2356c71f90ee63
Patcharaus-m <681110371@northbkk.ac.th>
2026-04-12 17:12:09 +0700

    feat:plan

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Axios instance with base URL pointing to the backend API
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://pmc-alwb.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
